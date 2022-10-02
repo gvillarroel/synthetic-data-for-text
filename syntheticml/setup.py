ab554c533cfa5398d7067a278367698888cf2d82
Gerardo Villarroel <villarroel.gj@gmail.com>
2022-10-02 12:03:42 -0300

    agregando data

---
+++ syntheticml/setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup, find_packages
+
+
+setup(
+    name='syntheticml',
+    version='0.0.1',
+    author='Gerardo Villarroel',
+    packages=find_packages(),
+
+
+    classifiers=[
+        'Environment :: Console'
+    ]
+
+)
